78d7652bef2921908cf4b8139cddd54222164f56
koirand <koirand.jp@gmail.com>
2020-03-20 00:47:27 +0900

    Update

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+# short alias
+alias c="docker-compose"
+alias d="docker"
+alias g="git"
+alias k="kubectl"
+alias y="yarn"
+
+# cheetsheet alias
+alias cs="vim ~/Projects/cheetsheet/cheet-sheet.md"
+
+# memo alias
+alias memo="vim + ~/Documents/memo.txt"
+
+# node env
+set -gx PATH $HOME/.nodebrew/current/bin $PATH
+
+# go env
+set -gx GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+set -gx GO111MODULE on
