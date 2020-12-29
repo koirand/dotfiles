01fc6c758b225a9df70525165805111dc8ba60cc
Kazuki Koide <koirand.jp@gmail.com>
2020-12-30 08:38:50 +0900

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,7 +14,7 @@ alias y="yarn"
 alias t="terraform"
 
 # cheetsheet
-alias cs="vim ~/src/github.com/koirand/cheetsheet/cheet-sheet.md"
+alias cs="vim ~/Documents/cheetsheet/cheet-sheet.md"
 
 # memo
 alias memo="vim + ~/Documents/memo.txt"
@@ -23,7 +23,7 @@ alias memo="vim + ~/Documents/memo.txt"
 alias launchpad-reset="defaults write com.apple.dock ResetLaunchPad -bool true;killall Dock"
 
 # nodebrew
-set -gx PATH $HOME/.nodebrew/current/bin $PATH
+# set -gx PATH $HOME/.nodebrew/current/bin $PATH
 # go
 set -gx GOPATH $HOME/go
 set -gx PATH $GOPATH/bin $PATH
